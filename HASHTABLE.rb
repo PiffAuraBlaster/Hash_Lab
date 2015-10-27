c39374ae48b1f4f7d52630285124723e0b40f98f
Kyle Napod <knnapod@puc.edu>
2015-10-27 15:17:06 -0700

    First Commit
    First commit framework for the HashTable Lab

---
+++ HASHTABLE.rb
@@ -0,0 +1,20 @@
+require 'bigdecimal'
+
+class MyHash
+  def initialize(p, w, A)
+  end
+  
+  def insert(key, val)
+    #Inserts a new hash value into the table and assigns it a key.
+  end
+  
+  def delete(key)
+    #Deletes a hash when given the key.
+  end
+  
+  def search(key)
+    #Finds a hash and value in hash table when given a key.
+  end
+  
+end
+
